033fa43456a9f8858882e996b6de97167b85183f
Mickey254-ja <mickey.awino@student.moringaschool.com>
2022-11-23 09:29:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Mikki Jade' ;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+
+const  excitedWelcomeMessage = `WELCOME TO FLATBOOK, MIKKI JADE!`;
+
+const shortGreeting = `Welcome, M!`;
